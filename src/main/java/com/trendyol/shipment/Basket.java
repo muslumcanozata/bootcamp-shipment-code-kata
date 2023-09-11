599081192521b01ee9cb15432933bdbda6b27022
mco <muslumcanozata@gmail.com>
2023-09-11 22:26:18 +0300

    implementing the basket final size logic

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,13 +1,44 @@
 package com.trendyol.shipment;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Basket {
 
     private List<Product> products;
 
     public ShipmentSize getShipmentSize() {
-        return null;
+        Map<ShipmentSize, Long> shipmentCounts = products.stream()
+                .collect(Collectors.groupingBy(Product::getSize, Collectors.counting()));
+
+        return checkIfProductSizeIsNotMoreThanThreeReturnMaxSize(shipmentCounts);
+    }
+
+    private ShipmentSize getMaxSizeFromTheBasket(Map<ShipmentSize, Long> shipmentCounts) {
+        return shipmentCounts.entrySet().stream()
+                .max(Map.Entry.comparingByKey())
+                .map(Map.Entry::getKey)
+                .orElse(ShipmentSize.X_LARGE);
+    }
+
+    private ShipmentSize checkIfItHasXLargeProductAndLessThanThreeProductInBasket(Map<ShipmentSize, Long> shipmentCounts) {
+        for (Map.Entry<ShipmentSize, Long> entry : shipmentCounts.entrySet()) {
+            if (entry.getValue() >= 3) {
+                return ShipmentSize.nextSize(entry.getKey()).orElseThrow(() -> new RuntimeException("Shipment size not found."));
+            }
+        }
+        return getMaxSizeFromTheBasket(shipmentCounts);
+    }
+
+    private ShipmentSize checkIfProductSizeIsNotMoreThanThreeReturnMaxSize(Map<ShipmentSize, Long> shipmentCounts) {
+        if (products.size() < 3) {
+            return shipmentCounts.entrySet().stream()
+                    .max(Map.Entry.comparingByKey())
+                    .map(Map.Entry::getKey)
+                    .orElse(ShipmentSize.X_LARGE);
+        }
+        return checkIfItHasXLargeProductAndLessThanThreeProductInBasket(shipmentCounts);
     }
 
     public List<Product> getProducts() {
@@ -17,4 +48,6 @@ public class Basket {
     public void setProducts(List<Product> products) {
         this.products = products;
     }
+
+
 }
--- src/main/java/com/trendyol/shipment/ShipmentSize.java
+++ src/main/java/com/trendyol/shipment/ShipmentSize.java
@@ -1,9 +1,32 @@
 package com.trendyol.shipment;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public enum ShipmentSize {
 
-    SMALL,
-    MEDIUM,
-    LARGE,
-    X_LARGE
+    SMALL(0),
+    MEDIUM(1),
+    LARGE(2),
+    X_LARGE(3);
+
+    private final int key;
+    ShipmentSize(int key) {
+        this.key = key;
+    }
+
+    public int getKey() {
+        return this.key;
+    }
+
+    public static Optional<ShipmentSize> nextSize(ShipmentSize size) {
+        if (ShipmentSize.X_LARGE.equals(size)) {
+            return Optional.of(size);
+        }
+        return getEnumByKey(size.getKey()+1);
+    }
+
+    public static Optional<ShipmentSize> getEnumByKey(int key) {
+        return Arrays.stream(ShipmentSize.values()).filter(shipmentSize -> shipmentSize.getKey() == key).findFirst();
+    }
 }
